c7c607a1725eba45d9afbf4de43a456b13bf9a7e
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-12-11 18:13:42 +0900

    fix anti gratifikasi and favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Layanan Konsultasi LPSPL - Sorong' }}</title>
+    <link rel="icon" href="{{ asset('img/logokkp.jpg') }}" type="image/jpeg">
     
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
